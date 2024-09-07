c8a4bd77425daa89fcae8962130888e91cc99631
MaxTernat0r <alabinmaxim@mail.ru>
2024-09-07 20:25:27 +0300

    task1 completed

--- _task1_.py
+++ _task1_.py
@@ -6,16 +6,13 @@ Y = []
 with open("datasets/dataset.csv", encoding='utf-8') as r_file:
     file_reader = csv.reader(r_file, delimiter=';')
     for row in file_reader:
-        X.append(row[0])
-        Y.append(row[1])
+        X.append(float(row[0]))
+        Y.append(float(row[1]))
 
 X = [[x] for x in X]
 
-print(X)
-print(Y)
-# regressor = LinearRegression()
-# regressor.fit(X, Y)
-#
-# print("k = ", regressor.coef_)
-# print("b = ", regressor.intercept_)
+regressor = LinearRegression()
+regressor.fit(X, Y)
 
+print("k =", *regressor.coef_)
+print("b =", regressor.intercept_)
