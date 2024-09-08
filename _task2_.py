bd8c8dcfdc80ca00477e862c509fc0a4cbab1ec5
MaxTernat0r <alabinmaxim@mail.ru>
2024-09-08 13:01:51 +0300

    task 2 completed

--- _task2_.py
+++ _task2_.py
@@ -1,8 +1,10 @@
 from sklearn.linear_model import LinearRegression
 import csv
+from sklearn import metrics
 
 X = []
 Y = []
+total = 0
 with open("datasets/boston.csv", encoding='utf-8') as r_file:
     file_reader = csv.reader(r_file, delimiter=',')
     cnt = 0
@@ -15,8 +17,12 @@ with open("datasets/boston.csv", encoding='utf-8') as r_file:
 
 regressor = LinearRegression()
 regressor.fit(X, Y)
+predicted_Y = regressor.predict(X)
 
+print(f'Предположительная стоимость квартир (medv): {Y}')
 
-print("k =", *regressor.coef_)
-print("b =", regressor.intercept_)
-
+for i in range(len(Y)):
+    total += Y[i]
+error = metrics.mean_squared_error(Y, predicted_Y) ** 0.5
+print(
+    f'Средняя ошибка предсказания равняется {metrics.mean_squared_error(Y, predicted_Y) ** 0.5}, что достаточно много, так как составляет примерно {error * 100 // (total / len(Y))}% от средней стоимости квартир.')
